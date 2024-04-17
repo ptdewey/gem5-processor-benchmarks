5ce125e2c294552a857a800ec460e7cd8ad254f3
Patrick Taylor Dewey <patrickdewey@sumac.rlogin>
2024-04-17 18:18:13 -0400

    fix(fixed image fetching script, modifications to run script)

--- get-dockcross-image.sh
+++ get-dockcross-image.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-if command -v docker >/dev/null 2>&1; then
-    echo "using docker..."
-    docker run --rm dockcross/linux-arm64 > dockcross-linux-arm64
-elif command -v podman >/dev/null 2>&1; then
+# NOTE: might need to manually pull image with tty
+if command -v podman >/dev/null 2>&1; then
     echo "using podman..."
     podman run --rm dockcross/linux-arm64 > dockcross-linux-arm64
+elif command -v docker >/dev/null 2>&1; then
+    echo "using docker..."
+    docker run --rm dockcross/linux-arm64 > dockcross-linux-arm64
 else
     echo "Error: neither Docker nor Podman is installed."
     exit 1
--- run.sh
+++ run.sh
@@ -1,38 +1,38 @@
 #!/usr/bin/env bash
 
-BINARY_DIR="build"
-OUTPUT_DIR="run_stats"
-
-# directories for different ISA builds
-ISA_MODELS=("X86" "ARM") # "POWER")
+build_dir="./build"
+output_dir="./run_stats"
 
 # NOTE: change as necessary
-PROCESSOR_TYPES=("TimingSimple" "O3")
+processor_types=("TimingSimple" "O3")
 
 run_simulation() {
     local binary_name=$1
     local isa=$2
     local processor_type=$3
-    local gem5_dir="gem5/build/${isa}"
-    local bp_script="main.py ${processor_type} ${binary_name}"
+    local gem5_dir="${HOME}/gem5_workspace/gem5/build/${isa}"
+    local bp_script="main.py ${binary_name} ${isa}"
     local stats_file="stats-${processor_type}-${binary_name}-${isa}.txt"
 
     echo "Running simulation for ${binary_name} with ISA ${isa} and processor type ${processor_type}..."
-    "${gem5_dir}/gem5.opt" --stats-file="${stats_file}" -d "${OUTPUT_DIR}" "${bp_script}" &
+    "${gem5_dir}/gem5.opt" --stats-file="${stats_file}" -d "${output_dir}" "${bp_script}" &
 }
 
-for isa in "${ISA_MODELS[@]}"; do
-    echo "Starting simulations for ISA ${isa}..."
-    # for processor_type in "${PROCESSOR_TYPES[@]}"; do
-        # echo "Using processor type ${processor_type}..."
-        for binary in "${BINARY_DIR}"/*; do
-            binary_name=$(basename "${binary}")
-            run_simulation "${binary_name}" "${isa}" # "${processor_type}"
-        done
-        wait
-        # echo "Completed all simulations for processor type ${processor_type}."
-    # done
-    echo "Completed simulations for ISA ${isa}."
+for isa_dir in "$build_dir"/*; do
+    isa_type=$(basename "$isa_dir")
+    for binary in "$isa_dir"/*; do
+        echo "Processing $binary for ISA $isa_type"
+        if [ "$isa_type" == "X86" ]; then
+            echo "Running X86 for $binary"
+            ./"$binary" &
+        elif [ "$isa_type" == "ARM" ]; then
+            echo "Running ARM for $binary"
+            ./"$binary" &
+        else
+            echo "Unknown ISA type for $binary"
+        fi
+    done
+    wait
 done
 
 echo "All simulations completed."
